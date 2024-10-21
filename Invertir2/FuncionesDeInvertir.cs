82db25b976d23808b916bffde4feb04c2b6b5824
JosephMoisesVargasRodriguez <vaecharsemae@gmail.com>
2024-10-20 20:04:15 -0600

    Mi trabajo

---
+++ Invertir2/FuncionesDeInvertir.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invertir2
+{
+      class ArregloDeFunciones
+      {
+            // Función para invertir el arreglo
+            public static int[] InvertirArreglo(int[] arreglo)
+            {
+                int[] invertido = new int[arreglo.Length];
+                int j = arreglo.Length - 1;
+
+                for (int i = 0; i < arreglo.Length; i++)
+                {
+                    invertido[j] = arreglo[i];
+                    j--;
+                }
+
+                return invertido;
+            }
+
+            // Función para contar los impares en el arreglo invertido
+            public static void ContarImpares(int[] arregloInvertido)
+            {
+                int conteoImpares = 0;
+
+                Console.WriteLine("\n-----------------------------------------");
+                Console.WriteLine("|Números impares en el arreglo invertido|");
+                Console.WriteLine("-----------------------------------------");
+
+                for (int i = 0; i < arregloInvertido.Length; i++)
+                {
+                    if (arregloInvertido[i] % 2 != 0)
+                    {
+                        Console.Write(arregloInvertido[i] + " ");
+                        conteoImpares++;
+                    }
+                }
+
+                Console.WriteLine("\nCantidad de números impares:");
+                Console.WriteLine($"{conteoImpares}");
+                Console.WriteLine("----------------------------------------");
+            }
+
+            // Función para mostrar el arreglo
+            public static void MostrarArreglo(int[] arreglo)
+            {
+                foreach (int num in arreglo)
+                {
+                    Console.Write(num + " ");
+                }
+                Console.WriteLine();
+            }
+      }
+}
---
+++ Invertir2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invertir2
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Solicitar al usuario la longitud del arreglo
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("|  ¿Cuántos números deseas ingresar?   |");
+            Console.WriteLine("----------------------------------------");
+
+            int n = int.Parse(Console.ReadLine());
+
+            // Crear el arreglo
+            int[] arreglo = new int[n];
+
+            // Solicitar los números al usuario
+            Console.WriteLine("\n----------------------------------------");
+            Console.WriteLine("| Ingresa los números:                 |");
+            Console.WriteLine("----------------------------------------");
+
+            for (int i = 0; i < n; i++)
+            {
+                int numero;
+                do
+                {
+                    Console.Write($"| Número {i + 1}: ");
+                    numero = int.Parse(Console.ReadLine());
+
+                    // Verificar si el número es negativo
+                    if (numero < 0)
+                    {
+                        Console.WriteLine("| Error: El numero es negativo. Inténtalo de nuevo.");
+                    }
+
+                } while (numero < 0);  // Repetir si el número es negativo
+
+                arreglo[i] = numero;
+            }
+
+            // Mostrar el arreglo original
+            Console.WriteLine("\n----------------------------------------");
+            Console.WriteLine("| Arreglo original:                    |");
+            Console.WriteLine("----------------------------------------");
+
+            ArregloDeFunciones.MostrarArreglo(arreglo);
+
+            // Invertir el arreglo
+            int[] arregloInvertido = ArregloDeFunciones.InvertirArreglo(arreglo);
+
+            // Mostrar el arreglo invertido
+            Console.WriteLine("\n----------------------------------------");
+            Console.WriteLine("| Arreglo invertido:                   |");
+            Console.WriteLine("----------------------------------------");
+
+            ArregloDeFunciones.MostrarArreglo(arregloInvertido);
+
+            // Contar e imprimir los impares en el arreglo invertido
+            ArregloDeFunciones.ContarImpares(arregloInvertido);
+
+            // Esperar que el usuario presione enter para finalizar
+            Console.WriteLine("Presiona enter para finalizar...");
+            Console.ReadKey();
+        }
+    }
+}
